b896fe3f5a12ed1cbb0399fc219e30cf7621266c
Allen Nakalema <allennakalema@gmail.com>
2026-03-24 23:49:15 +0300

    python-assignments

---
+++ allenn.py
@@ -0,0 +1,35 @@
+# Computer programming
+#Imagine you want to go somewhere and you need to use boda-boda, The boda-boda guy can not know where you are going and he can try guess but he can not tell the exact location , not until when you tell him, am heading to this location you guide them "Boss take me to town, take me to Buziga, use that shortcut", now imagine the computer is that boda-boda guy, but for the computer it can not guess,assume or even figure out things on its own, it only listens and follows exactly what you tell it to do, so computer programming is just the act of writing very clear instructions to a computer so that it can do something for you.
+
+# Memory partitioning
+#  Think of a large piece of land in the village, if anyone can come and just use it without boundaries, their will be a lot of chaos for example some one builds on your space and another person plants crops on your space. So this is what most people do, they divide the land into plots, and each person uses their own section, so this is the exact thing that happens in a computer, So basically memory partitioning means dividing the computer's memory into small units or sections so that different instructions can be stored their peacefully, so just as you are given a land title of your piece of land, these small unts of memory each is given a unique or specific memory address or identifier, this can easily help you locate which small unit you stored your instruction, just as you land title will help you identify where your piece of landis located and its dimensions.
+
+# Memory Address
+#now let's go deeper into memory address, Let's assume your in a village and someone tells you to go and call Sarah, you will ask yourself ,"which Sarah exactly, where does she stay?",according to these instructions you can hardly find Sarah. But if they tell you, Go to house number 12, near the borehole and call Sarah, now this is very clear, A memory address is like that house number, and everything in a computer is stored somewhere, and each location has its own address, so instead of saying , Bring that number or name I stored earlier, the computer says, Go to location 2027 and bring what is there.
+
+# How does RAM connects the CPU and the Hard Disk
+# Imagine you have a small business, and there is a warehouse or store "this is the hard disk", there is a table where work should be done from "this is RAM", and there is a worker doing the job "this is the CPU", now ask yourself can a worker do their work from inside the warehouse where everything is congested and packed tightly?, absolutely no, because its very slow and stressful with little work space, so the worker will do this,they will pick what they need from the warehouse, bring it on the table, works on it and then returns the things to the warehouse, so RAM connects the CPU and the hard disk by acting like a workspace.
+
+# A variable
+# So think of a pigy bank or a box, you can name it Money,and today you put inside it 10,000 inside it, and then tomorrow you remove the 10,000 and put 5,000, The pigy bank remained the same but the contents inside it were the ones changed, So a variable is just basically a named space in memory where you can store a value and you can change that value anytime you want
+
+# A statement 
+# Now think og giving your young broyher an instruction like, "start the bike, turn left, turn right,stop here", each one of these is a single complete instruction, So basically in programming, a statement is one single instruction that tells a computer to do something, it could be calculate something or store something etc.
+
+# Code
+# Now instead of just one instruction, imagine a full set of directions, like "atart the bike, go straight,  turn right, pick a customer, bring them to town and then collect the money", so the whole of this set of instuctions together is what we call code, code is simply many statements written together to achieve something, take it like a plan.
+
+# A block of code
+# So sometimes, instructions are grouped because they  belong together or they are connected, take an example of a boda-boda guy, as he starts his day he checks the fuel, starts the engine, and warm the bike These are seperate actions he is doing but they are all connected to preparing the bike for work, so in programming, that group of statements  that that work together to perform a specific task is what we call block of code.
+
+# List any 4 syntax of variable 
+# name = "Allen"
+# age = 41
+# price = 50000
+# num1 = 20.5
+
+# What is declaring? 
+# Declaring a variable simply means creating a variable.
+
+# What is initialising?  
+# Initialising basically means giving the variable a value, for example the variable age is given a value of 21, (age = 21), and in python we declare and initialize at the same time.
